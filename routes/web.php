cb541698ec5a647361bc3629cfd26b64ce7baa06
Damiano Bellamoli <damiano.bellamoli@hotmail.it>
2024-10-13 23:10:18 +0200

    routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Controller::class, 'welcome'])->name('welcome');
+Route::post('/', [UserController::class, 'showUserAction'])->name('showUserAction');
