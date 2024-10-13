4564848022891ab1cd9237e9cdb83c1b08a519ed
Damiano Bellamoli <damiano.bellamoli@hotmail.it>
2024-10-13 22:37:56 +0200

    base project files

---
+++ app/Providers/UserService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Providers;
+
+class UserService
+{
+    /**
+     * DO NOT MODIFY THIS METHOD
+     * ONLY THIS METHOD CAN READ data.txt
+     */
+    protected function getAllUsers()
+    {
+        $file = 'data/data.txt';
+
+        $data = json_decode(file_get_contents($file));
+
+        return $data;
+    }
+}
