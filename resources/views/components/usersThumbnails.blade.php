557b81ea3e2c0530fcde09739cabc87e370677d3
Damiano Bellamoli <damiano.bellamoli@hotmail.it>
2024-10-14 20:45:42 +0200

    tweaks to cards, mycss file

--- resources/views/components/usersThumbnails.blade.php
+++ resources/views/components/usersThumbnails.blade.php
@@ -1,11 +1,14 @@
-<div class="row">
+<div class="row justify-content-center">
     @foreach ($filteredUsers as $user)
-        <div class="card col-2 m-3">
-            <img src="<?= $user->resizedPicture ?>" class="card-img-top" alt="User picture <?= $user->id ?>">
-            <div class="card-body">
-                <p class="card-text"><?= ucfirst($user->name) ?></p>
-                <p class="card-text"><?= ucfirst($user->surname) ?></p>
-                <p class="card-text"><?= $user->last_login->format('Y-m-d H:i:s') ?></p>
+        <div class="card col-2 m-3 d-flex my_card">
+            <div class="card-body d-flex flex-column">
+                <a href="<?= $user->picture ?>" target="_blank"><img src="<?= $user->resizedPicture ?>" class="card-img-top mx-auto" style="width: 100px;"
+                    alt="User picture <?= $user->id ?>"></a>
+                <div class="mt-auto">
+                    <p class="card-text text-start m-0"><?= ucfirst($user->name) ?></p>
+                    <p class="card-text text-start m-0"><?= ucfirst($user->surname) ?></p>
+                    <p class="card-text text-start m-0"><?= $user->last_login->format('Y-m-d H:i:s') ?></p>
+                </div>
             </div>
         </div>
     @endforeach
