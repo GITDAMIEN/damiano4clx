1d5d8a29bda837b7769ddb21566d56f4129db2e3
Damiano Bellamoli <damiano.bellamoli@hotmail.it>
2024-10-14 21:02:04 +0200

    basic table style

--- resources/views/components/usersTable.blade.php
+++ resources/views/components/usersTable.blade.php
@@ -1,4 +1,4 @@
-<table class="table table-striped" id="usersTable">
+<table class="table table-striped table-bordered table-hover border-success" id="usersTable">
     <thead>
         <tr>
             <th>ID</th>
