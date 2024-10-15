32b3417460c70402b3ccc00c0a9a061144bc91e7
Damiano Bellamoli <damiano.bellamoli@hotmail.it>
2024-10-15 22:13:07 +0200

    table sorts

--- resources/js/myjs.js
+++ resources/js/myjs.js
@@ -5,5 +5,72 @@ $(function () {
         $('input').val('');
         $('select#active').val('NULL');
         $('select#view').val('table');
-    })
+    });
+
+    $('.ths').on('click', function () {
+        var order = $(this).hasClass('defaultSort') ? 'asc' : ($(this).hasClass('sortedDesc') ? 'asc' : 'desc');
+        var rows = $('#usersTable').find('tbody tr.userTr');
+        var columnClass = '.' + $(this).attr('data-targetTd');
+        var sortIcon = order === 'asc' ? '<i class="fa-solid fa-arrow-up-wide-short desc"></i>' : '<i class="fa-solid fa-arrow-down-wide-short desc"></i>';
+
+        if ($(this).hasClass('sortNumber')) {
+            sortNumberRows(rows, columnClass, order);
+        } else if ($(this).hasClass('sortString')) {
+            sortStringRows(rows, columnClass, order);
+        } else if ($(this).hasClass('sortDate')) {
+            sortDateRows(rows, columnClass, order);
+        } else if ($(this).hasClass('sortBool')) {
+            order = order === 'asc' ? 'desc' : 'asc';
+            sortStringRows(rows, columnClass, order);
+        }
+
+        $('.ths').find('i').remove();
+        if (!$(this).hasClass('defaultSort')) {
+            $(this).toggleClass('sortedDesc');
+        }
+        $('.ths').removeClass('defaultSort');
+        $('.ths').not(this).removeClass('sortedDesc');
+        $(this).append(sortIcon)
+    });
+
+    function sortNumberRows(rows, columnClass, order) {
+        rows.sort(function (a, b) {
+            if (order === 'asc') {
+                return parseFloat($(a).find(columnClass).text()) < parseFloat($(b).find(columnClass).text()) ? 1 : -1;
+            } else {
+                return parseFloat($(a).find(columnClass).text()) > parseFloat($(b).find(columnClass).text()) ? 1 : -1;
+            }
+        }).appendTo('#usersTable tbody');
+    }
+
+    function sortStringRows(rows, columnClass, order) {
+        rows.sort(function (a, b) {
+            if (order === 'asc') {
+                return $(b).find(columnClass).text().localeCompare($(a).find(columnClass).text());
+            } else {
+                return $(a).find(columnClass).text().localeCompare($(b).find(columnClass).text());
+            }
+        }).appendTo('#usersTable tbody');
+    }
+
+    function sortDateRows(rows, columnClass, order) {
+        rows.sort(function (a, b) {
+            var dateA = convertDate($(a).find(columnClass).text());
+            var dateB = convertDate($(b).find(columnClass).text());
+
+            if (order === 'asc') {
+                return dateB - dateA;
+            } else {
+                return dateA - dateB;
+            }
+        }).appendTo('#usersTable tbody');
+    }
+
+    function convertDate(dateTimeString) {
+        var [dateString, timeString] = dateTimeString.split(' ');
+        var newDateString = dateString.split('/').reverse().join('-');
+        var fullDateString = newDateString + ' ' + timeString;
+
+        return new Date(fullDateString);
+    }
 });
--- resources/views/components/usersTable.blade.php
+++ resources/views/components/usersTable.blade.php
@@ -1,26 +1,26 @@
 <table class="table table-striped table-bordered table-hover border-success" id="usersTable">
     <thead>
         <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Surname</th>
-            <th>Active</th>
-            <th>Last login</th>
+            <th class="ths sortNumber defaultSort" data-targetTd="idTd"><span>Id </span> <i class="fa-solid fa-arrow-down-wide-short asc"></i></th>
+            <th class="ths sortString" data-targetTd="nameTd"><span>Name </span></th>
+            <th class="ths sortString" data-targetTd="surnameTd"><span>Surname </span></th>
+            <th class="ths sortBool" data-targetTd="activeTd"><span>Active </span></th>
+            <th class="ths sortDate" data-targetTd="lastLoginTd"><span>Last login </span></th>
             <th>Picture</th>
-            <th>Rating</th>
+            <th class="ths sortNumber" data-targetTd="ratingTd"><span>Rating </span></th>
         </tr>
     </thead>
     <tbody>
         @foreach ($filteredUsers as $user)
             <tr class="userTr" data-userId="<?= $user->id ?>">
-                <td><?= $user->id ?></td>
-                <td><?= ucfirst($user->name) ?></td>
-                <td><?= ucfirst($user->surname) ?></td>
-                <td><?= $user->active == 1 ? 'Yes' : 'No' ?></td>
-                <td><?= $user->last_login->format('d/m/Y H:i:s') ?></td>
+                <td class="idTd"><?= $user->id ?></td>
+                <td class="nameTd"><?= ucfirst($user->name) ?></td>
+                <td class="surnameTd"><?= ucfirst($user->surname) ?></td>
+                <td class="activeTd"><?= $user->active == 1 ? 'Yes' : 'No' ?></td>
+                <td class="lastLoginTd"><?= $user->last_login->format('d/m/Y H:i:s') ?></td>
                 <td><a href="<?= $user->picture ?>" target="_blank"><img src="<?= $user->resizedPicture ?>"
                             alt="User picture <?= $user->id ?>"></a></td>
-                <td><?= $user->rating ?></td>
+                <td class="ratingTd"><?= $user->rating ?></td>
             </tr>
         @endforeach
     </tbody>
