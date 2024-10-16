4b017e505588f326e9af905705196617c30243cc
Damiano Bellamoli <damiano.bellamoli@hotmail.it>
2024-10-16 23:25:38 +0200

    header logo and tab icon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,12 +6,18 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>clxProject</title>
+    <link rel="shortcut icon" href="{{ asset('images/clxLogo.png') }}" alt="clxLogo" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
         integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
+<header>
+    <div class="row justify-content-center">
+        <img style="width: 120px;" src="{{ asset('images/clxLogo.png') }}" alt="clxLogo">
+    </div>
+</header>
 
 <body>
     {{ $slot }}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="title">clxProject</x-slot>
 
     <div class="container">
-        <div class="row my-5 text-center justify-content-center">
+        <div class="row my-2 text-center justify-content-center">
             <h1 id="welcomeHeader" class="my-5 text-my-green">Welcome to CLX Europe</h1>
             <h3 class="mb-4"><i class="fa-solid fa-filter" style="font-size: 20px;"></i> Filter users here:</h3>
             <form id="filtersForm" method="POST" action="{{ route('showUserAction') }}">
