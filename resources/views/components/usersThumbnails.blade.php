adc90740d59a12377d2a47d05600347434c14e69
Damiano Bellamoli <damiano.bellamoli@hotmail.it>
2024-10-16 23:41:57 +0200

    tweaks to cards

--- resources/views/components/usersThumbnails.blade.php
+++ resources/views/components/usersThumbnails.blade.php
@@ -7,9 +7,9 @@
                 <a href="<?= $user->picture ?>" target="_blank"><img src="<?= $user->resizedPicture ?>" class="card-img-top mx-auto" style="width: 100px;"
                     alt="User picture <?= $user->id ?>"></a>
                 <div class="mt-auto">
-                    <p class="card-text text-start m-0"><?= ucfirst($user->name) ?></p>
-                    <p class="card-text text-start m-0"><?= ucfirst($user->surname) ?></p>
-                    <p class="card-text text-start m-0"><?= $user->last_login->format('Y-m-d H:i:s') ?></p>
+                    <p class="card-text text-start m-0"><strong>Name:</strong> <?= ucfirst($user->name) ?></p>
+                    <p class="card-text text-start m-0"><strong>Surname:</strong> <?= ucfirst($user->surname) ?></p>
+                    <p class="card-text text-start m-0"><strong>Last login:</strong><br><?= $user->last_login->format('Y-m-d H:i:s') ?></p>
                 </div>
             </div>
         </div>
